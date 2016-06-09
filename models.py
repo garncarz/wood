075e529f2c22e604b97a83a9aa0b3ad853eb4556
Ondřej Garncarz <ondrej@garncarz.cz>
2016-06-09 06:40:29 +0200

    Model: side (ask/bid) added.

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import logging
 
 from sqlalchemy import (
-    Column, DateTime, Integer,
+    Column, DateTime, Enum, Integer,
     Table, ForeignKey,
 )
 from sqlalchemy.sql import func
@@ -11,15 +11,19 @@ from database import Base, db_engine, db_session
 logger = logging.getLogger(__name__)
 
 
-class Book(Base):
-    __tablename__ = 'book'
+class Order(Base):
+    __tablename__ = 'order'
 
     id = Column(Integer, primary_key=True)
+    side = Column(Enum('ask', 'bid'), nullable=False, index=True)
     price = Column(Integer, nullable=False, index=True)
     participant_id = Column(Integer, nullable=False)
     quantity = Column(Integer, nullable=False)
     created_at = Column(DateTime, nullable=False, index=True,
                         default=func.now())
+
+    def __repr__(self):
+        return '<Order %s/$%d/%d pcs>' % (self.side, self.price, self.quantity)
 
 
 def create_db():
